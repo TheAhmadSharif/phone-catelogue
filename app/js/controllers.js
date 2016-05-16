722520fdcd693114cd4cc876efd2f5627102fb4a
Ahmed Sharif <ahmadsharif@synchronousict.net>
2016-05-16 15:24:35 +0600

    Third commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var phonecatApp = angular.module('phonecatApp', [
-  'ngRoute'
+  'ngRoute',
+  'phonecatFilters'
 ]);
 
 /* Route */
@@ -17,6 +18,12 @@ phonecatApp.config(['$routeProvider',
         templateUrl: 'partials/details.html',
         controller: 'detailsController'
       }).
+      when('/about', {
+      	templateUrl: 'partials/about.html'
+      }).
+      when('/contact', {
+      	templateUrl: 'partials/contact.html'
+      }).
       otherwise({
         redirectTo: '/nothing-found',
         templateUrl: 'partials/404.html'
@@ -29,7 +36,10 @@ phonecatApp.config(['$routeProvider',
 
 phonecatApp.controller('PhoneListCtrl', ['$scope', '$routeParams','$http', function($scope, $routeParams, $http) {
 $scope.phoneOrder = 'age';
-		$http({method: 'get', url: 'phones/phones.json'}).then(function success(a){
+		$http({
+			method: 'get', 
+			url: 'phones/phones.json'
+		}).then(function success(a){
 				console.log(a);
 				$scope.phones = a.data;
 		}, function error(a){
@@ -43,13 +53,33 @@ phonecatApp.controller('detailsController',['$scope','$http','$routeParams',func
 	var path = $scope.name = $routeParams.cool;
 	console.log($routeParams.cool);
 
-	$http({method: 'get', url: 'phones/' + path + '.json'}).then(function success(Dataset){
+	$http({
+		method: 'get', 
+		url: 'phones/' + path + '.json'
+	}).then(function success(Dataset){
 			$scope.phone = Dataset.data;
 			console.log(Dataset);
+
+			$scope.focusImage = Dataset.data.images[0];
+
 	}, function error (){
 			console.log(Dataset);
 	});
 
 
+	$scope.setImage = function (imgUrl) {
+		console.log(imgUrl);
+		$scope.focusImage = imgUrl;
+	}
+
+
 
 }]);
+
+/* Filters */
+
+angular.module('phonecatFilters', []).filter('checkmark', function() {
+  return function(input) {
+    return input ? '\u2713' : '\u2718';
+  };
+});
